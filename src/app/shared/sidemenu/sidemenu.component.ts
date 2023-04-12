0d0fd2a81312a6d72f0764d3f574dda46f26c41e
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-04-12 16:40:27 -0600

    SideMenu

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface menuItem  {
+  texto: string;
+  ruta: string;
+}
+
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
@@ -7,4 +12,33 @@ import { Component } from '@angular/core';
 })
 export class SidemenuComponent {
 
+  templateMenu: menuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: 'template/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: 'template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: 'template/switches'
+    } 
+  ]
+
+  reactiveMenu: menuItem[] = [
+  {
+      texto: 'Básicos',
+      ruta: 'reactive/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: 'reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: 'reactive/switches'
+    } 
+  ]
 }
